0ae8dbf7f9d6f0694177099ef9c9259ac8b90ac8
ViktoriyaNefedova <vika.nefdova@list.ru>
2022-11-05 18:57:45 +0300

    calculator program

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <getopt.h>
 using namespace std;
+void help(){
+    cout<<"Неверное количество значений"<<endl;
+    exit(1);
+}
 int main(int argc, char *argv[])
 {
     if (argc==1) {
@@ -8,9 +12,16 @@ int main(int argc, char *argv[])
             <<"Для использования калькулятора нужно ввести один из параметров"<<endl
             <<"-m — умножение всех введенных значений" << endl
             <<"-d — деление первого значения на все последующие введенные значения"<< endl
+            <<"Количество значений от 4 до 6"<< endl
             <<"Для того чтобы продолжить запустите программу еще раз с нужным параметром и введите значения через пробел"<< endl;
     }
-    int opt, i, a=1, b, d;
+    int opt, i, a=1, b, d,st=0;
+    for (i=2; i<argc; i++){
+        st+=1;
+    }
+    if ((st > 2 and st<4) or (st>6)){
+        help();
+    }
     while ((opt = getopt (argc, argv, "d:m:")) != -1) {
         switch (opt) {
         case 'm':
@@ -31,3 +42,4 @@ int main(int argc, char *argv[])
         }
     }
 }
+
